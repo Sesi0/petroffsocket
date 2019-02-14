a1d3f5457861feb06ac3e812e74f98dc8e01456f
Borislav Nikolov <borislav.nikolov@petroffsoft.bg>
2019-02-15 00:08:42 +0200

    Updates

---
+++ precache-manifest.20cc2aca752498e050ba1d7cab5fdeaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbfc45e5a7ca7b1ebb06aded91d64e77",
+    "url": "/petroffsocket/static/media/logo.cbfc45e5.png"
+  },
+  {
+    "revision": "0369b4c5cf655c38a3c8",
+    "url": "/petroffsocket/static/js/runtime~main.0369b4c5.js"
+  },
+  {
+    "revision": "1c0cbc51bc40eb94b1f3",
+    "url": "/petroffsocket/static/js/main.1c0cbc51.chunk.js"
+  },
+  {
+    "revision": "e589dc4387052e568355",
+    "url": "/petroffsocket/static/js/2.e589dc43.chunk.js"
+  },
+  {
+    "revision": "1c0cbc51bc40eb94b1f3",
+    "url": "/petroffsocket/static/css/main.6b85e96e.chunk.css"
+  },
+  {
+    "revision": "e589dc4387052e568355",
+    "url": "/petroffsocket/static/css/2.453daf39.chunk.css"
+  },
+  {
+    "revision": "f266d39e9dfc46f8a13468ca8b96e860",
+    "url": "/petroffsocket/index.html"
+  }
+];
